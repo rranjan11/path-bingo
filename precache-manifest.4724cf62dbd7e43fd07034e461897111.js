61d060dce8b685397e04a688a6ed31da5ebf6169
rranjan11 <rishabhranjan461@gmail.com>
2026-03-05 03:10:33 -0500

    Updates

---
+++ precache-manifest.4724cf62dbd7e43fd07034e461897111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d04e50cbdff2ff3e49d0f1bae4f42ad",
+    "url": "/path-bingo/index.html"
+  },
+  {
+    "revision": "7e98eea193669e622f8d",
+    "url": "/path-bingo/static/css/main.cb5efdfc.chunk.css"
+  },
+  {
+    "revision": "1959a5b23b20f6a8cb9a",
+    "url": "/path-bingo/static/js/2.3a2a6e1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/path-bingo/static/js/2.3a2a6e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e98eea193669e622f8d",
+    "url": "/path-bingo/static/js/main.1780d65e.chunk.js"
+  },
+  {
+    "revision": "b1cc3c1719b91a73bf14",
+    "url": "/path-bingo/static/js/runtime-main.913d9d9b.js"
+  }
+]);
